4d0966ec11b6177d300408e4c4e6d973f10ae9ce
Mars7543 <miguelaroberts09@gmail.com>
2018-05-14 19:03:12 -0400

    Initial Commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const express     = require('express'),
+      app         = express();
+
+// app config
+app.set('view engine', 'ejs');
+app.use('/static', express.static(__dirname + '/public'));
+
+// route config
+const indexRoutes = require('./routes');
+
+app.use(indexRoutes);
+
+// server config
+const PORT = process.env.PORT || 8000;
+
+app.listen(PORT, () => {
+   console.log(`Server running on port ${PORT}...`);
+});
